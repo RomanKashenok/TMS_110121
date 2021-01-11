dbf5cccab0ec4fc53cdfc9fc9af9aea144fa8129
rkashenok <p7riu6dhgTK9sDuu>
2021-01-11 21:41:41 +0300

    hello world example

---
+++ lesson1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public static void main(String ... args) throws Exception {
+		
+		while(true) {
+			System.out.println("Hello TMS group world!!!");
+			Thread.sleep(1000);
+		}
+
+	}
+
+}
